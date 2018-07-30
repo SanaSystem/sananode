4c74ca7c3cd77d7c0a4a50059a021d42baee423c
alexromano87 <40470523+alexromano87@users.noreply.github.com>
2018-07-30 18:17:13 +0200

    Update urls.py

--- sananode/urls.py
+++ sananode/urls.py
@@ -20,5 +20,6 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
