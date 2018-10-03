f3c43a4c9fd079dab31dac42ceea56ceee129454
Sidharth Ramesh <tornadoalert@gmail.com>
2018-10-04 01:00:59 +0530

    changed ini files

--- sananode/settings.py
+++ sananode/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gs509tmqemor@6vn_zql0o(8gxjd#*6&ajm%g@dj5498dni)m0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
