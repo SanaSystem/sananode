eb3b08e9be7e259b6946ee79cd50b53c9cc434eb
Sidharth Ramesh <tornadoalert@gmail.com>
2018-10-19 18:08:48 +0530

    house keeping

--- initialize.py
+++ initialize.py
@@ -3,7 +3,6 @@ import requests
 import time
 import subprocess
 import os, sys
-import ifaddr
 banner = """
   ____    _    _   _    _      _   _  ___  ____  _____   _          _        
  / ___|  / \  | \ | |  / \    | \ | |/ _ \|  _ \| ____| | |__   ___| |_ __ _ 
@@ -18,16 +17,6 @@ def command(sequence):
         return ' '.join(sequence)
     else:
         return sequence
-
-def load_config():
-    with open("config.json","r") as f:
-        config = json.load(f)
-    return config
-
-def initialize_docker_compose():
-    with open("docker-compose.yml.sample", "r") as f:
-        docker_compose_yaml = yaml.load(f.read())
-        return docker_compose_yaml
 
 def wait_for_couch_container(couchdb_url=couchdb_unauth):
     attempt = 1
@@ -63,16 +52,6 @@ def wait_for_web_container(url="http://localhost:8000/"):
             time.sleep(t)
             attempt += 1 
 
-def write_docker_compose(yaml_data):
-    if 'docker-compose.yml' in os.listdir('.'):
-        answer = input("[?] Are you sure you want to overwrite the existing docker-compose.yml? (Y/n): ")
-        if answer.lower() not in ['yes','y']:
-            print("[-] Skipping writing docker-compose.yml")
-            return
-    with open("docker-compose.yml","w") as f:
-        print("[+] Writing docker-compose.yml")
-        f.write(yaml.dump(yaml_data))
-
 def initializa_couchdb(couch_username, couch_password):
     couchdb = "http://{}:{}@127.0.0.1:5984/".format(couch_username, couch_password)
     print("[+] Creating _users database")
@@ -100,6 +79,7 @@ def initializa_couchdb(couch_username, couch_password):
     },
     "language": "javascript"
     }
+    requests.put(couchdb + "medblocks/_design/preview", json=preview_medblock)
     # Create Preview
 
 
@@ -113,52 +93,13 @@ def initializa_couchdb(couch_username, couch_password):
     assert('_users' in dbs)
     assert('medblocks' in dbs)
 
-def get_ip():
-    print("[+] Scanning IP on all available interfaces")
-    results = [(adaptor.nice_name, adaptor.ips[-1].nice_name, adaptor.ips[-1].ip) for adaptor in ifaddr.get_adapters()]
-    print("[+] Found {} interfaces".format(len(results)))
-    data = [("Interface", "Name", "IP address")] + results
-    for i, d in enumerate(data):
-        line = '|'.join(str(x).ljust(45) for x in d)
-        print(line)
-        if i == 0:
-            print('-' * len(line))
-    public_ip = requests.get("http://ip.42.pl/raw").text
-    print("Public IP from ip.42.pl: {}".format(public_ip))
-    ip_list = [adaptor[2] for adaptor in results]
-    if public_ip not in ip_list:
-        print("-!-!- None of the interfaces match the Public IP. You will need to set up port forwarding to enable access outside the network -!-!-")
-    if input("Would you like to register your public IP {}?".format(public_ip)).lower() in ['yes','y']:
-        register_ip = public_ip
-    else:
-        register_ip = input("Please enter the IP address you want to configuire this SANA Node with (eg:{}): ".format(ip_list[0]))
-    print("[+] Registering node...")
-    bootstrap_url = "http://68.183.18.129:8000/nodes/"
-    r = requests.post(bootstrap_url, json={"ipAddress":register_ip, "couchReplication":True, "ipfsReplication":False})
-    if r.status_code == 201:
-        print("[+] Registered {} successfully".format(r.json()['ipAddress']))
-
 def bootstrap_ipfs():
     # Add bootstrap ip to ipfs bootstrap list
     return 
 
 def main():
     print(banner)
-    print("[+] Loading configuration from config.json")
-    config = load_config()
-    docker_compose_yaml = initialize_docker_compose()
     couch_username, couch_password = "admin", "admin"
-
-    if config['prompt']:
-        print("[o] Set up your couchDB username and Password")
-        couch_username = input("CouchDB Admin Username: ")
-        couch_password = input("CouchDB Admin Password: ")
-        couch_username_env = "COUCHDB_USER={}".format(couch_username)
-        couch_password_env = "COUCHDB_PASSWORD={}".format(couch_password)
-        docker_compose_yaml['services']['couchdb']['environment'] = [couch_username_env, couch_password_env]
-        
-    print("[+] Attempting to write docker-compose.yaml")
-    write_docker_compose(docker_compose_yaml)
     print("[+] Building docker containers")
     subprocess.call(command(["docker-compose","build"]), shell=True)
     print("[+] Running 'docker-compose up' as a child process. If this is the first time this may take a long time...")
@@ -177,7 +118,6 @@ def main():
 
     print("[+] Destroying containers")
     subprocess.call(command(["docker-compose", "down"]), shell=True)
-    get_ip()
 
 
     # Post the ip address on the Django databases (network URL)
