584b009bc072df71173b08c5185f207e67ea7cb2
Sidharth Ramesh <tornadoalert@gmail.com>
2018-10-20 19:14:03 +0530

    updated init

--- initialize.py
+++ initialize.py
@@ -2,7 +2,9 @@ import yaml, json
 import requests
 import time
 import subprocess
-import os, sys
+import os, sys, shutil
+import platform
+import decouple
 banner = """
   ____    _    _   _    _      _   _  ___  ____  _____   _          _        
  / ___|  / \  | \ | |  / \    | \ | |/ _ \|  _ \| ____| | |__   ___| |_ __ _ 
@@ -51,6 +53,8 @@ def wait_for_web_container(url="http://localhost:8000/"):
             print("[o] Waiting for containers to spin up...{} seconds".format(t))
             time.sleep(t)
             attempt += 1 
+def get_platform():
+    return platform.uname().machine
 
 def initializa_couchdb(couch_username, couch_password):
     couchdb = "http://{}:{}@127.0.0.1:5984/".format(couch_username, couch_password)
@@ -63,12 +67,13 @@ def initializa_couchdb(couch_username, couch_password):
     '_id': '_design/validate_medblocks',
     'validate_doc_update': 'function (newDoc, savedDoc, userCtx) {\nfunction require(field, message) {\nmessage = message || "Document must have a " + field;\nif (!newDoc[field]) throw({forbidden : message});\n};\nif (newDoc.type == "medblock") {\nrequire("format");\nrequire("files");\nrequire("keys");\nrequire("user");\n}\n}\n'
     }
-    requests.put(couchdb + "medblocks/_design/validate_medblocks", json=validate_medblock)
+    # requests.put(couchdb + "medblocks/_design/validate_medblocks", json=validate_medblock)
     authenticate_only_user = {
     "_id": "_design/only_user",
     "validate_doc_update": "function(newDoc, oldDoc, userCtx){\r\nif (newDoc.creator.email !== userCtx.name){\r\nthrow({forbidden : \"User must be the same as the one who created this document.\"});\r\n}\r\nif (oldDoc){\r\nif (oldDoc.creator.email !== userCtx.name){\r\nthrow({forbidden: 'User not authorized to modify.'});\r\n}\r\n}\r\n}\r\n"
     }
 
+    # requests.put(couchdb + "medblocks/_design/only_user", json=authenticate_only_user)
     preview_medblock = {
     "_id": "_design/preview",
     "views": {
@@ -80,13 +85,6 @@ def initializa_couchdb(couch_username, couch_password):
     "language": "javascript"
     }
     requests.put(couchdb + "medblocks/_design/preview", json=preview_medblock)
-    # Create Preview
-
-
-    
-    ## TO DO
-
-    requests.put(couchdb + "medblocks/_design/only_user", json=authenticate_only_user)
     print("Testing initialization...")
     dbs = requests.get(couchdb + "_all_dbs").json()
     # assert(len(dbs) == 2)
@@ -97,8 +95,34 @@ def bootstrap_ipfs():
     # Add bootstrap ip to ipfs bootstrap list
     return 
 
+def copy_env(platform):
+    platform = platform[:3].lower()
+    if platform not in ['amd', 'arm']:
+        print("[-] Not arm or amd...No .env matching .env file found")
+        return
+    if platform == 'amd':
+        print("[+] Copying environment file for AMD")
+        shutil.copy('amd.env','.env')
+    if platform == 'arm':
+        print("[+] Copying environment file for ARM")
+        shutil.copy('arm.env','.env')
+    
+
 def main():
     print(banner)
+    platform = get_platform()
+    print("[+] Machine architecture {}".format(platform))
+    if '.env' in os.listdir():
+        try:
+            prompt = input("[?] .env file already present. Overide with defaults?[Y/n]").lower()
+        except KeyboardInterrupt:
+            print("Interrupter by user. Exiting...")
+            exit()
+        if prompt in ["y","yes"]:
+            copy_env(platform)
+    else:
+        copy_env(platform)
+    
     couch_username, couch_password = "admin", "admin"
     print("[+] Building docker containers")
     subprocess.call(command(["docker-compose","build"]), shell=True)
@@ -119,10 +143,7 @@ def main():
     print("[+] Destroying containers")
     subprocess.call(command(["docker-compose", "down"]), shell=True)
 
-
     # Post the ip address on the Django databases (network URL)
-    
-
     print("Initialization done. Run node with 'docker-compose up'")
 
 if __name__ == '__main__':
